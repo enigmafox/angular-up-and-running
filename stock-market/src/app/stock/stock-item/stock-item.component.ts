11fa7b9e50d96619c53dd33fded14e8c0394829a
John Stone <elliotstone@gmail.com>
2020-05-24 18:25:37 -0400

    Finished chapter 3 - using angular directives.

--- stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -9,21 +9,36 @@ import { Stock } from '../../model/stock';
 })
 export class StockItemComponent implements OnInit {
 
-  public stock: Stock;
+  public stocks: Array<Stock>;
+  public stockClasses;
 
   constructor() { }
 
   ngOnInit(): void {
-      this.stock = new Stock(
+      this.stocks = [new Stock(
           'Test Stock Company',
           'TSC',
           85,
           80
-      );
+      ), new Stock(
+          'Second Stock Company',
+          'SSC',
+          10,
+          20
+      ), new Stock(
+          'Last Stock Company',
+          'LSC',
+          876,
+          765
+      )];
   }
 
-  toggleFavorite() {
-      this.stock.favorite = !this.stock.favorite;
+  toggleFavorite(index:number) {
+      this.stocks[index].favorite = !this.stocks[index].favorite;
+  }
+
+  trackStockByCode(index: number, stock:Stock) {
+      return stock.code;
   }
 
 }
